fd4412244570817c8b94297240b30a343e533023
woland7 <antonio.deiasio@gmail.com>
2020-05-27 13:46:29 +0200

    added move.sh script to selectively choose deployment

--- deployments/startup.sh
+++ deployments/startup.sh
@@ -3,4 +3,9 @@
 
 mv "/opt/jboss/wildfly/standalone/$DEPLOYMENT" /opt/jboss/wildfly/standalone/deployments/
 
+until cmd < /home/test/test.txt | grep 1; do
+  >&2 echo "MySQL is unavailable - sleeping 2s"
+  sleep 2
+done
+
 /opt/jboss/wildfly/bin/standalone.sh -b 0.0.0.0 -bmanagement 0.0.0.0
